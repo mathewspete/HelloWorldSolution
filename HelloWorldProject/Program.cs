565cff2230864d4b489664f47eb3f4172e9209e6
Pete Mathews <petemathews14@gmail.com>
2021-02-10 14:47:05 -0500

    Add project files.

---
+++ HelloWorldProject/Program.cs
@@ -0,0 +1,13 @@
+﻿//Notes
+using System;
+
+namespace HelloWorldProject {
+
+    class Program {
+
+        static void Main(string[] args) { // { Statement block beginning
+            Console.WriteLine("Hello World in C#!"); // Statement
+            Console.WriteLine("C# Written by Pete Mathews"); // Statement
+        } // Statement block end
+    }
+}
