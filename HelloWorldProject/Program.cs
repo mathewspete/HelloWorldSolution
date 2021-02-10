8bf0ce10c2d1aea79552e36fceadde9a85da1c45
Pete Mathews <petemathews14@gmail.com>
2021-02-10 15:00:01 -0500

    init commit

--- HelloWorldProject/Program.cs
+++ HelloWorldProject/Program.cs
@@ -5,9 +5,37 @@ namespace HelloWorldProject {
 
     class Program {
 
-        static void Main(string[] args) { // { Statement block beginning
-            Console.WriteLine("Hello World in C#!"); // Statement
-            Console.WriteLine("C# Written by Pete Mathews"); // Statement
-        } // Statement block end
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World in C#!");
+            Console.WriteLine("C# Written by Pete Mathews");
+
+            var counter = 0;
+            Console.WriteLine(counter);
+            counter += 13;
+            Console.WriteLine(counter);
+            counter -= 3;
+            Console.WriteLine(counter);
+            counter = counter * 5;
+            Console.WriteLine(counter);
+            var counterx = counter;
+            Console.WriteLine("counterx = " + counterx);
+            Console.WriteLine(counter);
+            counter = counter / 7 * 7;
+            Console.WriteLine(counter);
+            counter = counter / 7;
+            Console.WriteLine(counter);
+            counterx = counterx % 7;
+            Console.WriteLine(counterx);
+
+
+            var firstname = "Pete";
+            var lastname = "Mathews";
+            var fullname = firstname + " " + lastname;
+            Console.WriteLine(fullname);
+
+            fullname = fullname.ToUpper();
+            Console.WriteLine(fullname);
+
+        }
     }
 }
